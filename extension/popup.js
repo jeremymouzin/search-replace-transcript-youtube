34c61807356c39a89f290a100e99b129b0050f12
Jérémy Mouzin <jeremy.mouzin@gmail.com>
2020-01-28 15:24:38 +0100

    REFACTORING: improve code of popup.js + add french comments

--- extension/popup.js
+++ extension/popup.js
@@ -6,11 +6,13 @@ const replaceInput = document.getElementById('replace');
 const caseInsensitiveSearch = document.getElementById('insensitive-search');
 const preprocessButton = document.getElementById('preprocess-button');
 
-form.onsubmit = (element) => {
-  // Don't reload the page when submitting the form
-  element.preventDefault();
-
-  // Get input values from the form and send them to the content-script
+form.onsubmit = (event) => {
+  // 🇫🇷 Ne pas recharger la page lorsqu'on soumet le formulaire
+  // 🇬🇧 Don't reload the page when submitting the form
+  event.preventDefault();
+  
+  // 🇫🇷 Récupère les valeurs du formulaire et envoie-les au content-script
+  // 🇬🇧 Get input values from the form and send them to the content-script
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const searchExpression = searchInput.value;
     const replacementExpression = replaceInput.value;
@@ -19,7 +21,8 @@ form.onsubmit = (element) => {
     };
 
     chrome.tabs.sendMessage(tabs[0].id, { searchExpression, replacementExpression, options }, () => {
-      // Clean up the inputs so you can immediately do another search/replace without touching the mouse at all!
+      // 🇫🇷 UX: Efface les inputs pour pouvoir faire immédiatement une autre recherche sans toucher la souris du tout !
+      // 🇬🇧 UX: Clean up the inputs so you can immediately do another search/replace without touching the mouse at all!
       searchInput.value = '';
       replaceInput.value = '';
       searchInput.focus();
