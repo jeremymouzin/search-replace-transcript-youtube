7fe09159290d3adcaf24a6555f3090d6cf01bb1f
Jérémy Mouzin <jeremy.mouzin@gmail.com>
2020-01-28 16:02:47 +0100

    REFACTORING: Improve code and add french translation in options panel

--- extension/options.js
+++ extension/options.js
@@ -1,8 +1,9 @@
-// Let's grab useful elements
 const wordsList = document.getElementById('words-list');
-const autosaveMessage = document.querySelector('.autosave');
+const autosaveMessage = document.getElementById('autosave');
+const addButton = document.getElementById('add');
 
-// Saves options to chrome.storage
+// 🇫🇷 Sauvegarde la liste de mots dans le stockage local grâce à l'API chrome.storage
+// 🇬🇧 Save words list into local storage through chrome.storage API
 function saveOptions() {
   const list = document.querySelectorAll("li");
 
@@ -13,7 +14,8 @@ function saveOptions() {
 
     for (const item of list) {
       const [searchExpression, replacementExpression] = item.children;
-      // Only save non empty lines
+      // 🇫🇷 Sauvegarde uniquement les lignes non vides
+      // 🇬🇧 Save only non empty lines
       if (searchExpression.value.length || replacementExpression.value.length) {
         optionsToSave.wordsList.push([searchExpression.value, replacementExpression.value]);
       }
@@ -21,26 +23,33 @@ function saveOptions() {
 
     chrome.storage.sync.set(optionsToSave);
   } else {
-    // Delete everything that was previously saved
+    // 🇫🇷 S'il n'y a aucun mot dans la liste, supprimer tout du stockage
+    // 🇬🇧 If there is no words in the list, delete everything from the storage
     chrome.storage.sync.set({ wordsList: [] });
   }
 
-  // Flash for autosaving
   autosaveMessage.classList.remove('saving');
   autosaveMessage.textContent = "Done";
 }
 
-// Restores words list
+// 🇫🇷 Restaure la liste de mots depuis le stockage local
+// 🇬🇧 Restores the words list from the local storage
 function restoreOptions() {
   chrome.storage.sync.get({
     wordsList: [],
   }, function (items) {
+    // 🇫🇷 S'il n'y a aucun mot de stocké...
+    // 🇬🇧 If there is no stored word...
     if (items.wordsList.length <= 0) {
-      // UX: Add one empty line for the interface to get ready immediately
+      // 🇫🇷 UX: Ajouter une ligne vide pour faciliter la saisie
+      // 🇬🇧 UX: Add an empty line to facilitate input
       insertNewExpression();
-      // UX: Focus on the first input so the user can start typing right away 👌
+      // 🇫🇷 UX: Focus sur le premier input pour que l'utilisateur puisse taper immédiatement son mot
+      // 🇬🇧 UX: Focus on the first input so the user can start typing right away his word
       wordsList.firstElementChild.firstElementChild.focus();
     } else {
+      // 🇫🇷 S'il y a des mots stockés, on les ajoute à la liste
+      // 🇬🇧 If there are stored words, we add them to the list
       for (const [searchExpression, replacementExpression] of items.wordsList) {
         insertNewExpression(searchExpression, replacementExpression);
       }
@@ -48,61 +57,75 @@ function restoreOptions() {
   });
 }
 
-// Restores options as soon as the page is loaded
+// 🇫🇷 Restaure les options dès que la page est complètement chargée
+// 🇬🇧 Restores options as soon as the page is loaded
 document.addEventListener('DOMContentLoaded', restoreOptions);
 
-// Manage big plus button to add a new line
-document.getElementById('add').addEventListener('click', () => {
+// 🇫🇷 Gère le gros bouton plus pour ajouter une nouvelle ligne
+// 🇬🇧 Manage big plus button to add a new line
+addButton.addEventListener('click', () => {
   insertEmptyExpression();
 });
 
-// Manage delete button
-wordsList.addEventListener('click', function(e) {
+// 🇫🇷 Gère les boutons de suppression
+// 🇬🇧 Manage delete buttons
+wordsList.addEventListener('click', function (e) {
   if (e.target.matches('.trash-icon')) {
     e.target.parentElement.remove();
     saveOptions();
   }
 });
 
-// UX: Autosave every second after hitting a key
+// 🇫🇷 UX: Sauvegarde automatiquement après avoir tapé une touche
+// 🇬🇧 UX: Autosave after hitting a key
 let keyboardTimeoutId = 0;
-wordsList.addEventListener('keyup', function(event) {
-  // Don't save when moving around with the keyboard
+wordsList.addEventListener('keyup', function (event) {
+  // 🇫🇷 Ne sauvegarde pas quand on se déplace juste avec le clavier
+  // 🇬🇧 Don't save when moving around with the keyboard
   if (event.code !== 'Tab' && event.key !== 'Shift' && event.code !== 'Enter') {
     clearTimeout(keyboardTimeoutId);
     autosaveMessage.classList.add('saving');
     autosaveMessage.textContent = "Saving modifications...";
     keyboardTimeoutId = setTimeout(saveOptions, 500);
   }
-  
-  // UX: Ease the editing process with the use of ENTER key (avoid touching the mouse)
+
+  // 🇫🇷 UX: Facilite le processus d'édition grâce à la toucher ENTREE (pour éviter de toucher la souris)
+  // 🇬🇧 UX: Ease the editing process with the use of ENTER key (avoid touching the mouse)
   if (event.code === 'Enter') {
     if (event.target.matches('input[name="replacement-expression"]')) {
-      // If you edit the last line replacement expression and type ENTER it will add a new empty line
+      // 🇫🇷 Si tu édites l'expression de remplacement de la dernière ligne et tape ENTREE ça ajoutera une nouvelle ligne
+      // 🇬🇧 If you edit the last line replacement expression and type ENTER it will add a new empty line
       if (event.target.parentElement === wordsList.lastElementChild) {
         insertEmptyExpression();
       } else {
-        // If you're not on the last line, select and focus on the next line search expression
+        // 🇫🇷 Si on n'est pas sur la dernière ligne, sélectionne et focus sur l'expression de recherche de la ligne suivante
+        // 🇬🇧 If you're not on the last line, select and focus on the next line search expression
         event.target.parentElement.nextElementSibling.firstElementChild.select();
       }
     }
 
     if (event.target.matches('input[name="search-expression"]')) {
-      // Typing ENTER when editing a search expression will move you to the replacement expression
+      // 🇫🇷 Taper ENTREE quand on édite l'expression de recherche nous fait bouger sur l'expression de remplacement
+      // 🇬🇧 Typing ENTER when editing a search expression will move you to the replacement expression
       event.target.nextElementSibling.select();
     }
   }
 });
 
-// Add an empty line or an expression that was already saved previously
+// 🇫🇷 Ajoute une ligne vide ou une expression déjà sauvegardée précédemment
+// 🇬🇧 Add an empty line or an expression that was already saved previously
 function insertNewExpression(searchExpression = "", replacementExpression = "") {
-  wordsList.insertAdjacentHTML('beforeend', `<li><input type="text" placeholder="Search expression" value="${searchExpression}" name="search-expression"/>
-    →
-    <input type="text" placeholder="Replacement" value="${replacementExpression}" name="replacement-expression"/><button class="fas fa-trash trash-icon" tabindex="0"></button></li>
-    `);
+  wordsList.insertAdjacentHTML('beforeend', `
+<li>
+  <input type="text" placeholder="Search expression" value="${searchExpression}" name="search-expression"/>
+  →
+  <input type="text" placeholder="Replacement" value="${replacementExpression}" name="replacement-expression"/><button class="fas fa-trash trash-icon" tabindex="0"></button>
+</li>
+  `);
 }
 
-// UX: Focus immediately on the search expression to save one mouse click
+// 🇫🇷 UX: Focus immédiatement sur l'expression de recherche pour économiser un clic de souris
+// 🇬🇧 UX: Focus immediately on the search expression to save one mouse click
 function insertEmptyExpression() {
   insertNewExpression();
   wordsList.lastElementChild.firstElementChild.focus();
